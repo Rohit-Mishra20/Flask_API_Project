d9a260f6c2a0651e06b3a18b4149ac096079971a
Rohit-Mishra20 <mishrarohitmishra02@gmail.com>
2022-11-15 22:26:48 +0530

    Initial commit

---
+++ model/matrics_model.py
@@ -0,0 +1,33 @@
+import mysql.connector as my_con
+import json
+
+class matrics_model_class():
+    
+    def __init__(self):
+        try:
+            self.db_con=my_con.connect(host="localhost",user='root',password='',database='interview_project_db')
+            self.db_con.autocommit=True
+            self.cur=self.db_con.cursor(dictionary=True)
+            print("DB Connection Successful")
+        except Exception as e:
+            print("Error while connecting to DB",e)
+    def get_matrics_data_model(self,data):
+        start=data.get('start')
+        end=data.get('end')
+        print("start :",start,"end: ",end)
+        if start != None and end != None:
+            query=f"SELECT * FROM matrics_info_table where Time BETWEEN '{start}' and '{end}';"
+            self.cur.execute(query)
+            result=self.cur.fetchall()
+        else:
+            return json.dumps([{"error": "Please enter start & end","exp":"/metrics?start=<TS>&end=<TS>"}])
+
+        if len(result)>0:
+            return json.dumps(result, default=str)
+        else:
+            return json.dumps([{"error": "Data is not present"}])
+
+    def post_matrics_data_model(self,data):
+        #self.cur.execute(f"insert into users(Time,ID_1,ID_2) values({},{},{})")
+        pass
+
